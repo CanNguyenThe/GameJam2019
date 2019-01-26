55ae68b9826b6cdac120c5e48bd5071c1579e9b8
CHICKENB0Y93 <russty93@gmail.com>
2019-01-26 02:01:16 -0500

    Added health bar
    added health bar

---
+++ Assets/ImageTime.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageTime : MonoBehaviour {
+
+    
+    public Image tea;
+    public float timer = 0;
+    
+
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        timer = Time.time / 50;
+        var tempColor = tea.color;
+        tempColor.a = timer;
+        tea.color = tempColor;
+    }
+
+   
+}
